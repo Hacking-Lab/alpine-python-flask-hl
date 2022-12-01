91835aeffe90b934da5ebdb189c9be9d0a98183e
yuvaphalle <yuva.phalle@gmail.com>
2022-12-01 14:36:07 +0100

    Adding multi arch build script

---
+++ build-multi-arch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:latest . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:$1  . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:$1.0 . --push
