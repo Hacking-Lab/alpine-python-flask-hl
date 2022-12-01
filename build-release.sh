f2c8e954a014edc3dccbde0d1181bd72bc9557c9
yuvaphalle <yuva.phalle@gmail.com>
2022-12-01 14:31:24 +0100

    Updating build script

--- build-release.sh
+++ build-release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build --no-cache -t hackinglab/alpine-python-flask:$1.0 -t hackinglab/alpine-python-flask:$1 -t hackinglab/alpine-python-flask:latest -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-python-flask-hl:$1.0 -t hackinglab/alpine-python-flask-hl:$1 -t hackinglab/alpine-python-flask-hl:latest -f Dockerfile .
 
-docker push hackinglab/alpine-python-flask
-docker push hackinglab/alpine-python-flask:$1
-docker push hackinglab/alpine-python-flask:$1.0
+docker push hackinglab/alpine-python-flask-hl
+docker push hackinglab/alpine-python-flask-hl:$1
+docker push hackinglab/alpine-python-flask-hl:$1.0
 
 
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --no-cache -t hackinglab/alpine-python-flask:3.2.0 -t hackinglab/alpine-python-flask:3.2 -t hackinglab/alpine-python-flask:latest -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-python-flask-hl:3.2.0 -t hackinglab/alpine-python-flask-hl:3.2 -t hackinglab/alpine-python-flask-hl:latest -f Dockerfile .
 
