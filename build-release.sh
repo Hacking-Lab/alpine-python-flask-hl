3bf3e94322ec03a55e29e939894bb79a20b371dc
ibuetler <ivan.buetler@hacking-lab.com>
2023-10-25 23:18:10 +0200

    update image

--- build-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-python-flask-hl:$1.0 -t hackinglab/alpine-python-flask-hl:$1 -t hackinglab/alpine-python-flask-hl:latest -f Dockerfile .
-
-docker push hackinglab/alpine-python-flask-hl
-docker push hackinglab/alpine-python-flask-hl:$1
-docker push hackinglab/alpine-python-flask-hl:$1.0
-
-
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-python-flask-hl:3.2.0 -t hackinglab/alpine-python-flask-hl:3.2 -t hackinglab/alpine-python-flask-hl:latest -f Dockerfile .
-
