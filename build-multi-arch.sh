da4c031dbdaa4498ed8a2939e83ea1e1126b71e7
Ivan Bütler <ivan.buetler@hacking-lab.com>
2026-05-27 08:48:32 +0200

    update

--- build-multi-arch.sh
+++ build-multi-arch.sh
@@ -3,3 +3,7 @@
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:latest . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:$1  . --push
 docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask-hl:$1.0 . --push
+
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask:latest . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask:$1  . --push
+docker buildx build --platform linux/arm64,linux/amd64 -t hackinglab/alpine-python-flask:$1.0 . --push
